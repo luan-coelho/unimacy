f38be457493184fd53f59e18d7ccd8c7c488b5c3
Luan Coêlho <luancoelho.dev@gmail.com>
2022-06-14 23:51:28 -0300

    Jasper Vendas

---
+++ src/main/java/br/unitins/unimacy/controller/servelet/VendasReport.java
@@ -0,0 +1,30 @@
+package br.unitins.unimacy.controller.servelet;
+
+import java.sql.Connection;
+import java.util.HashMap;
+
+import javax.servlet.annotation.WebServlet;
+
+import br.unitins.unimacy.application.JDBCUtil;
+
+
+@WebServlet("/relatoriovendas.xhtml")
+public class VendasReport extends ReportServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getArquivoJasper() {
+		return "vendas.jasper";
+	}
+
+	@Override
+	public Connection getConnection() {
+		return JDBCUtil.getConnection();
+	}
+
+	@Override
+	public HashMap<String, Class<?>> getParametros() {
+		return null;
+	}
+}
